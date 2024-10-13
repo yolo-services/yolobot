b34a197b0eca2c6360aa8364b1a2cc46bf7031f2
fendziorr <xfendi.channel@gmail.com>
2024-10-14 01:38:23 +0200

    new level message fresh look

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,5 +1,7 @@
-const { Events } = require("discord.js");
+const { Events, EmbedBuilder } = require("discord.js");
 const UserLevel = require("../models/userLevel");
+const Guild = require("../models/guild");
+const mConfig = require("../messageConfig.json");
 
 module.exports = {
   name: Events.MessageCreate,
@@ -14,18 +16,34 @@ module.exports = {
       })) ||
       new UserLevel({ userId: message.author.id, guildId: message.guild.id });
 
-    userLevelData.exp += xpToAdd;
+    const guildData = await Guild.findOne({ guildId: message.guild.id });
 
+    userLevelData.exp += xpToAdd;
     const requiredXp = userLevelData.level * 100;
+    const logChannel = message.guild.channels.cache.get(
+      guildData.levelChannelId
+    );
+    const levelChannel = logChannel || message.channel;
 
     if (userLevelData.exp >= requiredXp) {
       userLevelData.level += 1;
-      userLevelData.exp = 0;
-      message.channel.send(
-        `**Congratulations!** ${message.author} leveled up to level **${userLevelData.level}**!`
-      );
+      userLevelData.exp -= requiredXp;
+      userLevelData.nextLevelExp = (userLevelData.level + 1) * 100;
+      const embed = new EmbedBuilder()
+        .setColor(mConfig.embedColorSuccess)
+        .setTitle("Level Up!")
+        .setDescription(
+          `**Congratulations!** ${message.author} leveled up to level **${userLevelData.level}**!`
+        )
+        .addFields({
+          name: "Next Level EXP",
+          value: `You now need **${
+            (userLevelData.level + 1) * 100
+          }** EXP for the next level!`,
+        })
+        .setTimestamp();
+      await levelChannel.send({ embeds: [embed] });
     }
-    userLevelData.nextLevelExp = requiredXp;
     await userLevelData.save();
   },
 };
