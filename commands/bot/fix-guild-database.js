cf3e1372c54c1efb43683459b2089dd9ea098634
fendziorr <xfendi.channel@gmail.com>
2025-07-02 11:08:59 +0200

    Add guild database fix command to handle existing guilds

---
+++ commands/bot/fix-guild-database.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const Guild = require("../../models/guild");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("fix-guild-database")
+    .setDescription("Fix the guild database error")
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+  async execute(client, interaction) {
+    const existing = await Guild.findOne({ guildId: guild.id });
+    if (existing) {
+      return interaction.reply({
+        content: "This server is already in the database. Nothing to do.",
+        ephemeral: true,
+      });
+    }
+
+    const newGuild = new Guild({
+      guildId: interaction.guild.id,
+    });
+
+    await newGuild.save();
+
+    await interaction.reply({
+      content:
+        "Guild added to database successfully. Everything is should be fine.",
+      ephemeral: true,
+    });
+  },
+};
--- events/guildCreate.js
+++ events/guildCreate.js
@@ -3,6 +3,9 @@ const Guild = require("../models/guild");
 module.exports = {
   name: "guildCreate",
   async execute(client, guild) {
+    const existing = await Guild.findOne({ guildId: guild.id });
+    if (existing) return;
+
     const newGuild = new Guild({
       guildId: guild.id,
     });
